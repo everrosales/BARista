93449e192d5ae82e6a0af710db861bdc8d6a1b95
Everardo Rosales <erosales@mit.edu>
2016-02-13 10:03:19 -0500

    Working on stuff.

--- app.js
+++ app.js
@@ -4,37 +4,8 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 var readline = require('readline');
 var logger = require('morgan');
-var Gmail = require('./utils/Gmail');
 
-var index = require('./routes/index');
-// var google = require('googleapis');
-// var googleAuth = require('google-auth-library');
-
-// Gmail goodies
-// var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
-// var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-//     process.env.USERPROFILE) + '/.credentials/';
-// var TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs-quickstart.json';
-
-// Load client secrets from a local file.
-/**
- * Process client secrets and stuff
- *
- */
-var secret_contents;
-
-fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the
-  // Gmail API.
-  //Gmail.authorize(JSON.parse(content), Gmail.listLabels);
-  secret_contents = content;
-  // Gmail.authorize(JSON.parse(content), Gmail.getUnreadEmailThreads);
-});
-
+var index = require('./routes/index');a
 
 var app = express();
 
@@ -49,6 +20,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 // app.use('/gmail', gmail);
+var WebSocketServer = require('websocket').server;
+wsServer = new WebSocketServer({
+  httpServer: server
+});
+
+wsServer.on('request', function(r) {
+  var connection = r.accept('echo-protocol', r.origin);
+  var count = 0;
+  var clients = {};
+  var id = count++;
+  clients[id] = connection;
+  console.log((new Date()) + ' Connection accepted [' + id + ']');
+  connection.on('message', function(message) {
+    var msgString = message.utf8Data;
+
+    console.log('Recieved msg: ' + msgString + ', by client: ' + id);
+  });
+})
 
 
 // catch 404 and forward to error handler
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,22 @@
 window.setInterval(function() {
 
 }, 60000)
+$('#orderDrink').click(function() {
+  console.log('order me a drink minion');
+})
+
+
+var WebSocketServer = require('websocket').server;
+wsServer = new WebSocketServer({
+  httpServer: server
+});
+
+wsServer.on('request', function(r) {
+  var connection = r.accept('echo-protocol', r.origin);
+  var count = 0;
+  var clients = {};
+  var id = count++;
+  clients[id] = connection;
+  console.log((new Date()) + ' Connection accepted [' + id + ']');
+
+})
